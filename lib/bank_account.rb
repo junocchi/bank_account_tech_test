c3bd0449318e6d9d73a69f5942d497fa227ed4db
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-02-21 21:51:19 +0000

    Fix tests (all passing) and implement bank_account

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,8 @@
+require 'date'
+
 class BankAccount
   attr_reader :transactions
+  attr_reader :balance
 
   def initialize
     @balance = 0
@@ -29,7 +32,27 @@ class BankAccount
       }
     end
   end
+
+  def format_statement
+    statement = "date || credit || debit || balance\n"
+    @transactions.reverse_each do |transaction|
+      date = transaction[:date].strftime('%d/%m/%Y')
+      credit = format('%.2f', transaction[:credit]) if transaction[:credit]
+      debit = format('%.2f', transaction[:debit]) if transaction[:debit]
+      balance = format('%.2f', transaction[:balance])
+      statement += "#{date} || #{credit || ''} || #{debit || ''} || #{balance}\n"
+    end
+    return statement
+  end
+
+  def print_statement
+    puts format_statement
+  end
 end
+
 
 # account = BankAccount.new
 # account.deposit(1000, Date.new(2023, 1, 10))
+# account.deposit(2000, Date.new(2023, 1, 13))
+# account.withdraw(500, Date.new(2023, 1, 14))
+# account.print_statement
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -5,8 +5,19 @@ RSpec.describe BankAccount do
   context "when a deposit is made into the account" do
     it "updates current balance" do
       account = BankAccount.new
-      deposit = account.deposit(1000, Date.new(2023, 1, 10))
-      expect( { :balance=>1000 }.has_key?(:balance)).to eq true
+      account.deposit(1000, Date.new(2023, 1, 10))
+      account.deposit(500, Date.new(2023, 1, 13))
+      expect(account.balance).to eq(1500)
+    end
+  end
+
+  context "when withdraw from account" do
+    it "updates current balance" do
+      account = BankAccount.new
+      account.deposit(1000, Date.new(2023, 1, 10))
+      account.deposit(500, Date.new(2023, 1, 13))
+      account.withdraw(750, Date.new(2023, 1, 14))
+      expect(account.balance).to eq(750)
     end
   end
 
@@ -15,6 +26,14 @@ RSpec.describe BankAccount do
       account = BankAccount.new
       account.deposit(1000, Date.new(2023, 1, 10))
       expect { account.withdraw(1100, Date.new(2023, 1, 12)) }.to raise_error "You don't have credit."
+    end
+  end
+
+  context "when withdraw from account" do
+    it "updates current balance" do
+      account = BankAccount.new
+      account.deposit(250, Date.new(2023, 1, 15))
+      expect(account.format_statement).to include("250.00")
     end
   end
 end
